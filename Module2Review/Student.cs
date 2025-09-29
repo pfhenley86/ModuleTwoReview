ffe0e19a188eb334a052e3d84b9b6a8bfedb4fd2
Ford Henley <info@fordswebhelp.com>
2025-09-29 18:03:11 -0400

    created two overloaded AddGrades methods

--- Module2Review/Student.cs
+++ Module2Review/Student.cs
@@ -7,10 +7,17 @@ public class Student
     public int Id { get; set; }
     public List<double> Grades { get; set; } = new List<double>{100.0, 90.0, 80.0, 70.0, 60.0};
 
-    // First Method To aDD gRADE
+    // First Method To add a grade
     public void AddGrade(double grade)
     {
       Grades.Add(grade);  
+    }
+    
+    //Second method to add a grade with overloading
+
+    public void AddGrade(params double[] grades)
+    {
+        Grades.AddRange(grades);
     }
     
     public Student()
