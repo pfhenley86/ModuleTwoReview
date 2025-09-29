b5622133fbcfb79911f8f76864264a2a22e44302
Ford Henley <info@fordswebhelp.com>
2025-09-29 18:32:41 -0400

    Completed review exercie

--- Module2Review/Program.cs
+++ Module2Review/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         //Create Students List
-        var students = new List<double>();
+        List<Student> students = new List<Student>();
         
         //Create four instances of the Student Class
         
@@ -15,6 +15,7 @@ class Program
         studentOne.Name = "John";
         studentOne.Id = 1;
         studentOne.AddGrade(100.0, 70.0, 80.0);
+        students.Add(studentOne);
         
         //Instance Two
         Student studentTwo = new Student();
@@ -22,6 +23,7 @@ class Program
         studentTwo.Name = "Jenny";
         studentTwo.Id = 2;
         studentTwo.AddGrade(95.0, 73.0, 64.0);
+        students.Add(studentTwo);
         
         //Instance Three
         Student studentThree = new Student();
@@ -29,6 +31,7 @@ class Program
         studentThree.Name = "Mary";
         studentThree.Id = 3;
         studentThree.AddGrade(88.0, 90.0, 86.0);
+        students.Add(studentThree);
         
         //Instance Four
         Student studentFour = new Student();
@@ -36,5 +39,22 @@ class Program
         studentFour.Name = "Jack";
         studentFour.Id = 4;
         studentFour.AddGrade(93.0, 87.0, 78.0);
+        students.Add(studentFour);
+        
+        //Loop Through List of Students
+        foreach (Student student in students)
+        {
+            Console.WriteLine($"ID: {student.Id}, Name: {student.Name}");
+            
+            Console.Write("Grades: ");
+            foreach (double grade in student.Grades)
+            {
+                Console.Write($"{grade} ");
+            }
+
+            Console.WriteLine(); // New line after grades
+            Console.WriteLine($"Average Grade: {student.CalculateAverageGrade():F2}");
+            Console.WriteLine(); // Extra space between students
+        }
     }
 }
